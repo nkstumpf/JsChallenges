7380f42623275ab286987ea38829f79855da08e3
nkenon <37303352+nkenon89@users.noreply.github.com>
2019-08-06 00:13:16 -0400

    working on challenge 07

--- 07ConsoleQuiz/app.js
+++ 07ConsoleQuiz/app.js
@@ -23,3 +23,59 @@ var quiz = {
         'Louis Pasteur'
     ]
 }
+
+
+// create a function constructor called "question"
+// question should include the q
+
+/* 
+
+1. question itself
+2. the answers from which the player can chose the corrrect one
+3. the correct answer (use a number)
+
+*/
+
+var Question = function(question, correctAnswer) {
+    this.question = question;
+    this.answers = quiz.answers;
+    this.correctAnswer = correctAnswer
+};
+
+// create a couple of questions using the constructor 
+
+var questionOne = new Question(quiz.questions[0], quiz.answers[0]);
+var questionTwo = new Question(quiz.questions[1], quiz.answers[1]);
+var questionThree = new Question(quiz.questions[2], quiz.answers[2]);
+var questionFour = new Question(quiz.questions[3], quiz.answers[3]);
+var questionFive = new Question(quiz.questions[4], quiz.answers[4]);
+var questionSix = new Question(quiz.questions[5], quiz.answers[5]);
+var questionSeven = new Question(quiz.questions[6], quiz.answers[6]);
+var questionEight = new Question(quiz.questions[7], quiz.answers[7]);
+var questionNine = new Question(quiz.questions[8], quiz.answers[8]);
+var questionTen = new Question(quiz.questions[9], quiz.answers[9]);
+
+// store the questions inside an array
+
+var myQuestions = [
+    questionOne, 
+    questionTwo, 
+    questionThree, 
+    questionFour, 
+    questionFive,
+    questionSix,
+    questionSeven,
+    questionEight,
+    questionNine,
+    questionTen
+];
+
+// select one random question and log it the console together with the possible answers
+
+function randomQuestion(arr) {
+    x = Math.floor(Math.random() * 10);
+    console.log(arr[x]);
+}
+
+// use the prompt function to ask the user for the correct answer
+
--- 07ConsoleQuiz/examples.js
+++ 07ConsoleQuiz/examples.js
@@ -1,4 +1,4 @@
-// Object creation
+// Standard object creation
 
 var newCar = {
     make: 'hyundai',
@@ -38,32 +38,178 @@ Car.prototype.calculateAge = function () {
 // inheritable property
 Car.prototype.engineType = '4 cylynder';
 
+// Object.create
+// This is another way to create objects
+
+var carProto = {
+    calculateAge: function() {
+        console.log(2019 - this.year);
+    }
+};
+
+// builds an object that inherits directly from the first argument we pass in
+// create a variable using the object.create method and pass it the carProto object as an argument
+var yourNewCar = Object.create(carProto);
+yourNewCar.make = 'land rover';
+yourNewCar.model = 'range rover';
+yourNewCar.year = 2020;
+
+// you can also pass the object.create funtion another parameter like this in order to fill the object with data of your choosing 
+var anotherNewCar = Object.create(carProto, {
+    make: {value: 'toyota'},
+    model: {value: '4 runner'},
+    year: {value: '2019'}
+})
+
+// First Class Functions
+
+/*
+
+- A function is an instance of the object type 
+- A function behaves like any other object
+- We can store functions in a variable
+- We can pass a function as an argumnet to another function
+- We can return a function from a function
+
+In computer science, a programming language is said to support "first-class functions" (or function literal) if it treats functions as first-class objects. Specifically, this means that the language supports constructing new functions during the execution of a program, storing them in data structures, passing them as arguments to other functions, and returning them as the values of other functions.
+
+*/
+
+// Passing functions as arguments
+
+var years = [1990, 1965, 2019, 2016, 2007]; // a given array that you might want to run some calculations on later
 
 
+// VERY USEFUL!
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
 
-
-var quiz = {
-    questions: [
-        'Who was the legendary Benedictine monk who invented champagne?',
-        'Name the largest freshwater lake in the world?',
-        'What is someone who shoes horses called?',
-        'What item of clothing was named after its Scottish inventor?',
-        'What kind of weapon is a falchion?',
-        'Which word goes before vest, beans and quartet?',
-        'What is another word for lexicon?',
-        'Name the seventh planet from the sun.',
-        'Who invented the rabies vaccination?',
-    ],
-    answers: [
-        'Dom Perignon',
-        'Lake Superior',
-        'The Moon',
-        'Farrier',
-        'Makintosh',
-        'Sword',
-        'String',
-        'Dictionary',
-        'Uranus',
-        'Louis Pasteur'
-    ]
+function arrayCalc(arr, fn) { // create a function that takes the array as well as the function that will execute the calculation on that array as arguments
+    var arrResult = []; // this will hold the result
+    for (var i = 0; i < arr.length; i++) { // iterate throught the array
+        arrResult.push(fn(arr[i])); // push the current iteration of the loop into the arguments of the passed funtions
+    }
+    return arrResult; 
 }
+
+// this function will be passed in as a callback argument to the above function
+function calcAge(passedElement) {
+    return 2019 - passedElement;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+
+
+// and here we call the first function...
+
+// pass in the desired array AND the desired function ON that array
+var carAges = arrayCalc(years, calcAge);
+// console log the results
+console.log("your cars are this many years old: " + carAges);
+
+// we can perform whatever function we want on any array using this procedure 
+// such as checking which cars are brand new...
+
+function isNew(passedElement) {
+    return passedElement >= 2019;
+}
+
+var newCars = arrayCalc(years, isNew);
+console.log(newCars);
+
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+
+// Functions returning functions
+
+function interviewQuestion(job) { // the first function takes a parameter
+    if (job === 'designer') { // and... returns another function that takes a second parameter
+        return function(name) {
+            // code goes here
+            console.log(name + ', Can you please explain what UX design is?');
+        } 
+    } else if (job === 'teacher') {
+        return function(name) {
+        console.log(name + ' What subject do you teach?');
+        }
+    } else {
+        return function(name) {
+            console.log('Hello ' + name + ', what do you do?');
+        }
+    }
+}
+
+var teacherQuestion = interviewQuestion('teacher');
+var designerQuestion = interviewQuestion('designer');
+
+designerQuestion('Mark');
+teacherQuestion('John');
+
+//////////////////////////////////////////////////////////////////////////////
+// Still dont quite undersand how this works...
+
+function carType(type) {
+    if (type === 'coupe') {
+        // function
+        return function(carMake) {
+        console.log('Your ' + carMake + ' has 2 doors!');
+        }
+    } else if (type === 'sedan') {
+        return function(carMake) {
+            console.log('Your ' + carMake + ' has 4 doors!');
+        }
+    } else {
+        return function(carMake) {
+            console.log('We arent sure how many doors this car type has');
+        }
+    }
+}
+
+// basically you can use this technique to return a number of different results fron the same function based on the parameters you pass in later?
+
+var theCarType = carType('coupe');
+
+theCarType('Honda');
+
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+
+// Function Expressions (immediately invoked function expressions IIFE)
+
+// Dont understand this concept at all...
+
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+
+// Closures
+
+// Closure is when a function (child), inside another function (parent), has access to its parent variables. The inverse does not apply, a parent function cannot see variables of its children functions.
+
+// in regard to closures... An inner function always has access to the variables and parameters of its outer funciton, even after the outer function has returned
+
+function retirement(retirementAge) {
+    var a = ' years left until retirement';
+    return function(yearOfBirth) {
+        var age = 2016 - yearOfBirth;
+        console.log((retirementAge - age) + a );
+    }
+}
+
+retirementUS = retirement(66);
+retirementUS(1990);
+
+// Rewrite a previous example function utilizing closures
+
+function interviewQuestion(job) { // because of closures, the nested function will have access to the "job" argument even after the first function has executed
+    return function(name) {
+        if (job === 'designer') {
+                console.log(name + ', Can you please explain what UX design is?');
+        } else if (job === 'teacher') {
+            console.log(name + ' What subject do you teach?');
+        } else {
+                console.log('Hello ' + name + ', what do you do?');
+            }
+        }
+    }
+
