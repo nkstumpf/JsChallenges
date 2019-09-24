25b27e88a7bc3fd0b5c4fbbf9aeba9f24094ed42
nkenon <37303352+nkenon89@users.noreply.github.com>
2019-09-24 01:08:29 -0400

    new project started

--- 00Examples/examples.js
+++ 00Examples/examples.js
@@ -229,6 +229,38 @@ function interviewQuestion(job) { // because of closures, the nested function wi
 
 
 /////////////////////////
+// CALL
+/////////////////////////
+
+// Call allows you to call a specific function on a specific object without using a protoype
+
+
+// example
+
+// define an object
+var myObject = {
+    randomNumber: 2
+};
+
+// define a function
+var myFunction = function(a, b) {
+    this.randomNumber + a + b;
+};
+
+console.log('///////// CALL EXAMPLE /////////')
+console.log(myFunction.call(myObject, 2, 2));
+
+
+
+
+
+/////////////////////////
+// APPLY
+/////////////////////////
+
+// Apply works the same way as "call" except it allows you to pass an array as an argument
+
+/////////////////////////
 // BIND 
 /////////////////////////
 
@@ -258,357 +290,7 @@ var john = {
 
 
 
-/////////////////////////
-// CALL
-/////////////////////////
 
-// Call allows you to call a specific function 
-
-
-
-
-
-/////////////////////////
-// APPLY
-/////////////////////////
-
-// Apply works the same way as "call" except it allows you to pass an array as an argument
-
-
-
-/////////////////////////
-// ARRAYS IN ES6
-/////////////////////////
-
-// ES6 
-
-
-
-/////////////////////////
-// THE SPREAD OPERATOR
-/////////////////////////
-
-// the spread operator (...arr) is used to display/call all the elements within an array
-
-
-/////////////////////////
-// REST PARAMETERS
-/////////////////////////
-
-// same syntax as the spread operator (...variable) except it is used on a variable to convert the arguments of a function to an accessible array
-
-//ES5 example
-
-// This is an example of how you would have to this in ES5
-
-function isFullAges5() {
-    // console.log('these are the arguments' + arguments)
-    var argsArr = Array.prototype.slice.call(arguments);
-
-    /* what the above line is doing ^
-
-    1. create a variable named: argsArr
-    2. go into the Array.prototype constructor
-    3. slice... aka: separate "these" elements...
-    4. "these" being the arguments that are being "called"
-    5. store them inside the argsArr variable
-
-    */
-
-   console.log('Rest Params - ES5 EXAMPLE:');
-
-    argsArr.forEach(function(current) {
-        console.log((2016 - current) >= 18);
-    });
-};
-
-isFullAges5(1990, 1999, 1965);
-
-//ES6 example
-
-// and this is an example of how you would do the same thing in ES6...
-
-function isFullAges6(...years) {  // "years" does not exist as an array yet - it is simply a placeholder argument for values that will be passed into the function as array like arguments.
-    // console.log(years);
-
-    console.log('Rets Params - ES6 EXAMPLE:');
-
-    years.forEach(current => console.log((2016 - current >= 18)));
-};
-
-isFullAges6(1990, 1999, 1965);
-
-/////////////////////////
-// REST PARAMETERS PART II
-/////////////////////////
-
-// same syntax as the spread operator (...variable) except it is used on a variable to convert the arguments of a function to an accessible array
-
-//ES5 example
-
-// This is an example of how you would have to this in ES5
-
-function isFullAge5(limit) { // limit = age limit
-
-    /* 
-    
-    So if we were to change this function to accept an argument instead of being anonymous, ie: "limit" we would then also have to add a "limit" in as an argument when we call the function. Doing so would mess up our function because the way we have it written below we are telling the browser to make an array of ALL the arguments returned from prototype - not JUST the ones we are wanting to check the full age of. 
-    
-    */
-
-
-    // console.log('these are the arguments' + arguments)
-    var argsArr = Array.prototype.slice.call(arguments, 1); // add 1 here to indicate to the slice method to start slicing at position 1 which will exclude the first argument because its at position 0
-
-   console.log('Rest Params - ES5 EXAMPLE II:');
-
-    argsArr.forEach(function(current) {
-        console.log((2016 - current) >= limit); // change the hard coded limit number to the argument limit
-    });
-};
-
-isFullAge5(21, 1990, 1999, 1965); // 16 is the "limit" argument
-
-//ES6 example
-
-/* 
-
-With ES6 however this is not an issue becuase we can simply "add" an additional argument alongside the rest parameters and it will differentiate them by identifying the "..." as saying "group all of these arguments together as one".
-
-*/
-
-function isFullAge6(limit, ...years) {
-
-    console.log('Rets Params - ES6 EXAMPLE II:');
-
-    years.forEach(current => console.log((2016 - current >= 18)));
-};
-
-isFullAge6(21, 1990, 1999, 1965);
-
-/////////////////////////
-// DEFAULT PARAMETERS
-/////////////////////////
-
-/* 
-
-Default parameters are the parameters of a contructor function that are defaulted to when the parameters are not explicitly defined.
-
-*/
-
-// ES5 example
-
-function Familymember(firstName, yearOfBirth, lastName, nationality) {
-
-    // in ES5 we have to define the default parameters with an if else statement or a ternary operator like this:
-
-    firstName === undefined ? firstName = 'John' : firstName = firstName;
-
-    lastName === undefined ? lastName = 'Doe' : lastName = lastName;
-
-    nationality === undefined ? nationality = 'American' : nationality = nationality;
-
-
-    this.firsName = firstName;
-    this.lastName = lastName;
-    this.yearOfBirth = yearOfBirth;
-    this.nationality = nationality;
-};
-
-var nick = new Familymember('Nick', 1989, 'Stumpf', 'German');
-var randomPerson = new Familymember('Jane', 2000);
-
-// ES6
-
-// ES6 makes setting default parameters much easier by allowing us to define the default parameters right inside the contructor like this:
-
-function AnotherFamilyMember(firstName = 'Jane', lastName = 'Doe', yearOfBirth = 1999, nationality = 'American') {
-
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.yearOfBirth = yearOfBirth;
-    this.nationality = nationality;
-};
-
-var anotherRandomPerson = new AnotherFamilyMember();
-
-
-/////////////////////////
-// MAPS
-/////////////////////////
-
-// Hash mapping: new key/ value data structure in ES6
-
-/*
-
-Map is a contructor function that take creates a data structure out of key / value pairs that you "set" to it and then maps those pairs to an array
-
-*/
-
-const question = new Map();
-
-
-// set method
-// used to set the data
-question.set('question', 'What is the official name of the latest major javascript version?'); // key - value pair
-
-question.set(1, 'ES5');
-question.set(2, 'ES6');
-question.set(3, 'ES2015');
-question.set(4, 'ES7');
-question.set(5, 'I will be deleted');
-question.set('correct', 3);
-question.set(true, 'Correct!');
-question.set(false, 'Nope :(');
-
-// get method
-// used to retrieve the data
-console.log(question.get('question'));
-console.log(question.get(1));
-
-// other useful methods
-
-// size method
-// used to retrieve the "size" of the map structure (number of entries)
-console.log(question.size);
-
-// delete method 
-// deletes data from the structure
-question.delete(5);
-
-// has method
-// checks if the data structure "has" an certain element
-if (question.has(4)) {
-    console.log('I have this!');
-} else console.log('I don\'t have this');
-
-// clear method
-// clears all data from the structure
-// question.clear();
-
-// Iterating through map using forEach
-
-// question.forEach((value, key) =>
-//     console.log(`this is ${key}, and it\'s value is set to ${value}`));
-
-// For of loop
-
-// ****** GO BACK AND REVIEW THIS *******
-
-// we can accomplish the same result using a for of loop.
-
-/* 
-
-In this block of code we are looping through the data structure. Destructuring (the bracket notation) is used to target the data we want - the key and value pairs of all question entries. Then we are saying "if the type of value stored inside key is a number execute this code"
-
-*/
-
-for (let [key, value] of question.entries()) {
-    if (typeof(key) === 'number') {
-        console.log(`Answer ${key}: ${value}`);
-    }
-}
-
-// prompting for our quiz answer
-
-/* 
-
-what this block of code is doing is saying set a constant to the variable name "answer" and store within it a prompt (user input) that asks you to input the correct answer and then stores that input to the variable. 
-
-Below that is a line that says "go get..." a string within the data structure called "correct" and that equals (===) the correct answer
-
-*/
-
-const answer = parseInt(prompt('Please enter the correct answer'));
-    question.get(answer === question.get('correct'));
-
-
-/////////////////////////
-// CLASSES
-/////////////////////////
-
-
-// ES5 example:
-
-var Person5 = function(name, yearOfBirth, job) {
-    this.name = name;
-    this.yearOfbirth = yearOfBirth;
-    this.job = job;
-}
-
-Person5.prototype.calculateAge = function() {
-    var age = new Date().getFullYear - this.yearOfBirth;
-    console.log(age);
-}
-
-// Classes are just the ES6 way of creating constructor funtions
- 
-/* Main diferences between classes in ES6 and constructors is ES5 are:
-
-1. No hoisting in classes
-2. Can only use methods with classes not properties
-
-*/
-
-/////////////////////////
-// SUB CLASSES
-/////////////////////////
-
-
-// ES5 example:
-
-// Constructor function ie: class
-
-var Person5 = function(name, yearOfBirth, job) {
-    this.name = name;
-    this.yearOfbirth = yearOfBirth;
-    this.job = job;
-}
-
-// add a method to the new objects prototype:
-
-Person5.prototype.calculateAge = function() {
-    var age = new Date().getFullYear() - this.yearOfBirth;
-    console.log(age);
-}
-
-// create a new person using the class
-var john5 = new Person5('John', 1990, 'teacher');
-
-// Sub Class
-var Athlete5 = function(name, yearOfBirth, job, olympicGames, medals) {
-
-    Person5.call(this, name, yearOfBirth, job);
-
-    /* 
-    
-    Here we are "calling" the Person class and asigning the "this" keyword to point to Athlete5. Otherwise it will point to an empty object?
-
-    */
-
-    this.olympicGames = olympicGames;
-    this.medals = medals;
-
-    // Then we set the other properties as usual
-
-}
-
-Athlete5.prototype = Object.create(Person5.prototype);
-
-// This line "connects" the athlete subclass to the person class so that they can share the same prototype
-
-
-// ES6 example:
-
-class Person6 {
-    constructor(name, yearOfBirth, job) {
-        this.name = name;
-        this.yearOfBirth = yearOfBirth;
-        this.job = job;
-    } // no punctuation needed here
-
-    calculateAge()
-}
 
 
 
---
+++ 16GradeBook/script.js
@@ -0,0 +1,29 @@
+/*
+
+Planning the app
+
+What needs to happen when we push the submit button?
+
+* Create a data structure to store our data that can be organized for averaging grades and different students
+
+1. Event listener
+
+2. Get the value of each input field and store it
+
+3. push data to data stucture
+
+4. we need methods of that data stucture that will return the data we want and update is accordingly
+
+5. get the dom elements where we want to post the data 
+
+6. post the data to the correct dom elements
+
+7. add a 'remove item' button that gets created with the new data
+
+// extra feature ideas: 
+
+// add the ability to add new students to your class and store all their data separately
+// add dropdown menu with all the students in your class that when selected would bring up that students scores
+// add ability to sort your table by subject or grade
+
+*/
